cad7c70a46c4b5a0eedcff214e08a227152ac5c7
Vassili Baranov <vassili.baranov@gmail.com>
2018-05-10 23:12:44 +0300

    error correction

--- memcards/models.py
+++ memcards/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 
 
 class QuestionList(models.Model):
-    name = models.CharField(maxlength=100)
+    name = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Question(models.Model):
-    question_list = models.ForeingKey(QuestionList, on_delete=models.CASCADE)
+    question_list = models.ForeignKey(QuestionList, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.TextField()
     answer = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
