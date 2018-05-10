302a42ef000c7eb3403bd5177720af8243e09343
Vassili Baranov <vassili.baranov@gmail.com>
2018-05-10 22:33:33 +0300

    Add QuestionList and Question models

--- memcards/models.py
+++ memcards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionList(models.Model):
+    name = models.CharField(maxlength=100)
+    created = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    question_list = models.ForeingKey(QuestionList, on_delete=models.CASCADE)
+    question = models.TextField()
+    answer = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
