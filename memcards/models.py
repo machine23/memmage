854b6c9bcaeaaba162e6f8b9a03987c304faeac5
Vassili Baranov <vassili.baranov@gmail.com>
2018-05-11 09:32:21 +0300

    split Question model into a Question and Answer

--- memcards/models.py
+++ memcards/models.py
@@ -12,11 +12,21 @@ class Tag(models.Model):
 class Question(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
-    question = models.TextField()
-    answer = models.TextField()
+    text = models.TextField()
     group = models.IntegerField()  # memory card group
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.question
+        return self.text
+
+
+class Answer(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
