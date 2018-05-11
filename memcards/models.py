87288ab40cac9a66150bc867364d7acca988710c
Vassili Baranov <vassili.baranov@gmail.com>
2018-05-11 09:18:00 +0300

    add Tag model

--- memcards/models.py
+++ memcards/models.py
@@ -1,17 +1,22 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
-class QuestionList(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_now_add=True)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Question(models.Model):
-    question_list = models.ForeignKey(QuestionList, on_delete=models.CASCADE)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
     question = models.TextField()
     answer = models.TextField()
+    group = models.IntegerField()  # memory card group
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
